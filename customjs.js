ea20694629de08ef47c6af39ad189140c919f227
mako <mahmutgulen34@gmail.com>
2023-10-30 16:21:01 +0300

    finish

--- customjs.js
+++ customjs.js
@@ -6,6 +6,8 @@ const wordText = document.getElementById('wordText');
 const meaning = document.getElementById('result-meaning');
 const audio = document.getElementById('audio');
 
+const noResults=document.getElementById('noResults');
+
 
 async function fetchApi() {
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${wordInput.value}`;
@@ -16,12 +18,14 @@ async function fetchApi() {
 
     if (result.title) {
         resultDiv.style.display = 'none';
+        noResults.style.visibility='visible';
     }
     else {
         resultDiv.style.display = 'block';
         wordText.textContent = result[0].word;
         meaning.textContent = result[0].meanings[0].definitions[0].definition;
         audio.src = result[0].phonetics[0].audio;
+        noResults.style.visibility='hidden';
     }
 
 }
